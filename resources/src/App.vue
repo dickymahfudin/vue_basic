40d8388470bf2b3bee28dc288ea2fc03f890dc97
dickymahfudin <dickymahfudin13@gmail.com>
2020-03-26 08:56:29 +0700

    Revert "edit name"
    This reverts commit df14ce3ea90478478971e531eef18450a55d3fa6.

--- resources/src/App.vue
+++ resources/src/App.vue
@@ -3,7 +3,7 @@
     <div class="kiri">
       <div class="logo">
         <a href="https://wegodev.com">
-          <h2>Dism</h2>
+          <h2>Wegodev</h2>
           <span>Notes</span>
         </a>
       </div>
